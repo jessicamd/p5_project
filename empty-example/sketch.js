a867696f22cb27110e070246fbe6b95ce735414f
Jessica Dowdall <jessicadowdall@calvisitor-10-105-130-108.calvisitor.1918.berkeley.edu>
2019-09-29 13:59:32 -0700

    everything now moves

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,145 +1,146 @@
-var board;
+gridSpeed = 5; //this is all for board
+xDirection = 1;
+yDirection = 1;
+band = 250; //so doesn't overlap with game pieces' starting position
 
 function setup(){
-  frameRate(10); //testing needed
-  createCanvas(screen.width, screen.height); //fullscreen
-  board = new Grid();
-  board.size = 10;
-  x1 = new X(50, 50);  //this is hideous, better code?
-  x2 = new X();
-  x3 = new X();
-  x4 = new X();
-  x5 = new X();
-  o1 = new O();
-  o2 = new O();
-  o3 = new O();
-  o4 = new O();
-  o5 = new O();
-  var xPieces = [x1, x2, x3, x4, x5];
-  var oPieces = [o1, o2, o3, o4, o5];
-  var controlX = 0;
-  var controlO = 0;
+  //frameRate(10); //testing needed
+  //createCanvas(windowWidth, windowHeight); //fullwindow
+  board = new Grid(0, 250);
+
+  x1 = new X();
+  x2 = new X(150);
+  x3 = new X(250);
+  x4 = new X(350);
+  x5 = new X(450);
+  xArray = [x1, x2, x3, x4, x5];
+  ix = 0;
+
+  o1 = new O(75, 150);
+  o2 = new O(175, 150);
+  o3 = new O(275, 150);
+  o4 = new O(375, 150);
+  o5 = new O(475, 150);
+  oArray = [o1, o2, o3, o4, o5];
+  io = 0;
+
 }
 
 function draw(){
+  createCanvas(windowWidth, windowHeight); //fullwindow
   background(255);
+
   board.display();
   board.move();
-//  x1.display();
-//  xPieces.display(); //could this work? or need to display separately? could create while loop to make it easier to increase # of pieces
-//  yPieces.display(); //and this? maybe display().apply(yPieces)?
-/**
-  xPieces[controlX].keyPressedX();
-  if(keyCode === 81 || keyCode === 69){ //81 = Q and 69 = E
-    controlX += 1;
-  }
-  oPieces[controlO].keyPressedO();
-  if(keyCode === 16){ //shift-right
-    controlO += 1;
-  }
-  **/
+
+  x1.display();
+  x2.display();
+  x3.display();
+  x4.display();
+  x5.display();
+  xArray[ix].move();
+
+  o1.display();
+  o2.display();
+  o3.display();
+  o4.display();
+  o5.display();
+  oArray[io].move();
+
 }
 
-//board class, have tried it as function and class, multiple iterations below, none will display even an ellipse
 class Grid{
-  constructor(size, x, y){
-    this.size = size;
+  constructor(x = 50, y = 50){
     this.x = x;
     this.y = y;
   }
   display(){
-    ellipse(10, 10, this.size, this.size);
-    /**
-
-    **/
+    line(this.x + 100, this.y, this.x + 100, this.y + 300);
+    line(this.x + 200, this.y, this.x + 200, this.y + 300);
+    line(this.x, this.y + 100, this.x + 300, this.y + 100);
+    line(this.x, this.y + 200, this.x + 300, this.y + 200);
   }
-  move(){ //should make sure board never leaves canvas and moves in random increments <= 5 in x and y directions
-    if(this.x < screen.width - this.size/2 - 5) {
-      this.x += Math.floor(Math.random() * 6);
-      return this.x;
+  move(){
+    if(this.y > height - 300 - gridSpeed){
+      yDirection = -1;
     }
-    else if(this.x > this.size/2 + 5){
-      this.x -= Math.floor(Math.random() * 6);
-      return this.x;
+    if(this.y < gridSpeed + band){
+      yDirection = 1;
     }
-    if(this.y < screen.height - this.size/2 - 5) {
-      this.y += Math.floor(Math.random() * 6);
-      return this.y;
+    this.y += gridSpeed * yDirection;
+    if(this.x > width - 300 - gridSpeed){
+      xDirection = -1;
     }
-    else if(this.y > this.size/2 +5){
-      this.y -= Math.floor(Math.random() * 6);
-      return this.y;
+    if(this.x < gridSpeed){
+      xDirection = 1;
     }
+    this.x += gridSpeed * xDirection;
   }
 }
-/**
-  this.size = size;
-  this.display = function(){
-    ellipse(50, 50, this.size, this.size);
-  }
-**/
-/**
-class Grid(){
-  constructor(size){
-    this.s = size;
-    this.cells = []; //don't really understand how this works, array?
-    this.cSize = (width - 1)/this.s
-  }
-  display(){
-    let cSize = this.cSize;
-    this.cells.forEach(function(element){
-      rect(element.x = 0, element.y = 0, cSize, cSize);
-    });
-  }
-}
-**/
 
 class X{
-  constructor(x, y){
+  constructor(x = 50, y = 50){
     this.x = x;
     this.y = y;
   }
   display(){
-    return "X";
+    line(this.x, this.y, 50 + this.x, 50 + this.y);
+    line(this.x + 50, this.y, this.x, this.y + 50);
+  }
+  move(){
+    if(keyIsDown(UP_ARROW) && this.y >= 10){ //87 = W, keycode.info
+      console.log("yay!");
+      this.y -= 10;
+    }
+    else if(keyIsDown(DOWN_ARROW) && this.y <= height - 50 - 10){ //S
+      this.y += 10;
+    }
+    if(keyIsDown(RIGHT_ARROW) && this.x <= width - 50 - 10){ //D
+      this.x += 10;
+    }
+    else if(keyIsDown(LEFT_ARROW) && this.x >= 10){ //A
+      this.x -= 10;
+    }
   }
 }
 
 class O{
-  constructor(x, y){
+  constructor(x = 50, y = 50){
     this.x = x;
     this.y = y;
   }
   display(){
-    return "O";
+    ellipse(this.x, this.y, 2 * sqrt(1250)); //sqrt(25^2 + 25^2) is height of x's
+  }
+  move(){
+    if(keyIsDown(87) && this.y >= sqrt(1250) + 10){ //87 = W, keycode.info
+      console.log("yay!");
+      this.y -= 10;
+      console.log(this.y);
+    }
+    else if(keyIsDown(83) && this.y <= height - sqrt(1250) - 10){ //S
+      this.y += 10;
+    }
+    if(keyIsDown(68) && this.x <= width - sqrt(1250)){ //D
+      this.x += 10;
+    }
+    else if(keyIsDown(65) && this.x >= sqrt(1250)){ //A
+      this.x -= 10;
+    }
   }
 }
 
-function keyPressedX(){
-  if(keyCode === UP_ARROW){
-    this.y += 10;
+function keyPressed(){
+  if(keyCode === 16){
+    if(ix >= xArray.length - 1){
+      ix = 0;
+    }
+    else ix += 1;
   }
-  else if(keyCode === DOWN_ARROW){
-    this.y -= 10;
-  }
-  if(keyCode === RIGHT_ARROW){
-    this.x += 10;
-  }
-  else if(keyCode === LEFT_ARROW){
-    this.x -= 10;
-  }
-}
-
-function keyPressedO(){
-  if(keyCode === 87){ //87 = W, keycode.info
-    this.y += 10;
-  }
-  else if(keyCode === 83){ //S
-    this.y -= 10;
-  }
-  if(keyCode === 68){ //D
-    this.x += 10;
-  }
-  else if(keyCode === 65){ //A
-    this.x -= 10;
+  if(keyCode === 81 || keyCode === 69){ //Q = 81, E = 69
+    if(io >= oArray.length - 1){
+      io = 0;
+    }
+    else io += 1;
   }
 }
